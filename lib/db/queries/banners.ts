84f1d5298cde428b310240d3a7f3273c70fb70f2
wongvc92 <wongvuichung92@gmail.com>
2024-10-09 21:31:00 +0800

    fix code

--- lib/db/queries/banners.ts
+++ lib/db/queries/banners.ts
@@ -1,20 +1,25 @@
 import { IbannerImage } from "@/lib/types";
+import { unstable_cache } from "next/cache";
 
 const baseUrl = process.env.NEXT_PUBLIC_ADMIN_URL!;
 
-export const getBanners = async (): Promise<IbannerImage[] | null> => {
-  const url = new URL(`${baseUrl}/api/banners`);
-  try {
-    const res = await fetch(url.toString(), { next: { tags: ["banners"] } });
+export const getBanners = unstable_cache(
+  async (): Promise<IbannerImage[] | null> => {
+    const url = new URL(`${baseUrl}/api/banners`);
+    try {
+      const res = await fetch(url.toString());
 
-    if (!res.ok) {
+      if (!res.ok) {
+        return null;
+      }
+      const data = await res.json();
+
+      return data.bannerImages;
+    } catch (error) {
+      console.error("Failed fetch banner images");
       return null;
     }
-    const data = await res.json();
-
-    return data.bannerImages;
-  } catch (error) {
-    console.error("Failed fetch banner images");
-    return null;
-  }
-};
+  },
+  ["banners"],
+  { tags: ["banners"] }
+);
