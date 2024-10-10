e7b40c30c45f0f83716b18db0c8c6b19dbc1d978
wongvc92 <wongvuichung92@gmail.com>
2024-10-10 12:19:03 +0800

    fix code

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import TagsLink from "@/components/tags-link";
 import { Skeleton } from "@/components/ui/skeleton";
 import ImageSliderLoading from "@/components/loading/image-slider-loading";
 
+export const revalidate = 0;
 export default async function Home() {
   return (
     <main className="w-full min-h-screen md:container py-10">
