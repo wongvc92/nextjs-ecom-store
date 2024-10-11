055ae8422c9fa26dd6f1c91ca80da5e48b1e3a05
wongvc92 <wongvuichung92@gmail.com>
2024-10-11 15:16:27 +0800

    test isr without tag

--- lib/db/queries/filters.ts
+++ lib/db/queries/filters.ts
@@ -40,9 +40,7 @@ export const getSizes = async () => {
 export const getCategories = async () => {
   const url = new URL(`${baseUrl}/api/filter/categories`);
   try {
-    const response = await fetch(url.toString(), {
-      next: { tags: ["categories"] },
-    });
+    const response = await fetch(url.toString());
     if (!response.ok) {
       return [];
     }
