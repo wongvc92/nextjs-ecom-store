043eebbcf3db1a9b59065b9bf36d68f75e64564c
wongvc92 <wongvuichung92@gmail.com>
2024-10-11 16:41:14 +0800

    test isr product page by id

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -2,21 +2,20 @@ import { NextRequest, NextResponse } from "next/server";
 import { revalidatePath } from "next/cache";
 
 export async function POST(req: NextRequest) {
-  const { searchParams } = new URL(req.url);
-  const secret = searchParams.get("secret");
-  const paths = searchParams.getAll("path");
+  const data = await req.json();
+  const { secret, urlPaths } = data;
 
   if (secret !== process.env.NEXT_PUBLIC_REVALIDATE_SECRET) {
     return NextResponse.json({ message: "Invalid token" }, { status: 401 });
   }
-  if (!paths) {
+  if (!urlPaths) {
     return NextResponse.json({ message: "Please provide paths" }, { status: 401 });
   }
   try {
     // Revalidate the specified path
     await Promise.all(
-      paths.map(async (path) => {
-        revalidatePath(path, "page");
+      urlPaths.map(async (path: string) => {
+        revalidatePath(path);
       })
     );
     return NextResponse.json({ revalidated: true });
